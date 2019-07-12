df82ee92be6801df0c407169e2c833497771c6d6
Name <you@example.com>
2019-07-12 19:15:08 +0200

    Add InputModelValidators

--- src/TimeTracker/Startup.cs
+++ src/TimeTracker/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using TimeTracker.Data;
+using TimeTracker.Models.Validation;
 
 namespace TimeTracker
 {
@@ -31,7 +33,8 @@ namespace TimeTracker
             services.AddDbContext<TimeTrackerDbContext>(options =>
                 options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddControllers();
+            services.AddControllers()
+                .AddFluentValidation(options => options.RegisterValidatorsFromAssemblyContaining<UserInputModelValidator>());
 
 
 
